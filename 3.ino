68ff55dfaab156ab1f24616f297d4a2b923f9a3f
S Harish <46412734+harishsdev@users.noreply.github.com>
2022-05-24 14:14:25 +0530

    Add files via upload

---
+++ 3.ino
@@ -0,0 +1,53 @@
+void setup() {
+
+  Serial.begin(112500);
+  char task1Param[12] = "task1Param";
+  char task2Param[12] = "task2Param";
+  /* we create a new task here */
+  xTaskCreate(
+      task1,           /* Task function. */
+      "task1",        /* name of task. */
+      10000,                    /* Stack size of task */
+      (void *)task1Param,                     /* parameter of the task */
+      1,                        /* priority of the task */
+      NULL);                    /* Task handle to keep track of created task */
+  xTaskCreate(
+      task2,           /* Task function. */
+      "task2",        /* name of task. */
+      10000,                    /* Stack size of task */
+      (void *)task1Param,                     /* parameter of the task */
+      2,                        /* priority of the task */
+      NULL);                    /* Task handle to keep track of created task */ 
+      
+}
+
+/* the forever loop() function is invoked by Arduino ESP32 loopTask */
+void loop() {
+
+}
+/* this function will be invoked when additionalTask was created */
+void task1( void * parameter )
+{
+  Serial.println((char *)parameter);
+  /* loop forever */
+  for(;;){
+    Serial.println("task1 is running");
+    delay(1000);
+  }
+  /* delete a task when finish, 
+  this will never happen because this is infinity loop */
+  vTaskDelete( NULL );
+}
+
+void task2( void * parameter )
+{
+  Serial.println((char *)parameter);
+  /* loop forever */
+  for(;;){
+    Serial.println("task2 is running");
+    delay(1000);
+  }
+  /* delete a task when finish, 
+  this will never happen because this is infinity loop */
+  vTaskDelete( NULL );
+}
